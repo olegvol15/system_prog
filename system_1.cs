a0d76fc68cfdc518f6c7b5c81b387f2f7b19aff6
olegvol15 <122642067+olegvol15@users.noreply.github.com>
2024-07-04 16:11:13 +0200

    Add files via upload

---
+++ system_1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+//class Program
+//{
+//    static async Task Main(string[] args)
+//    {
+//        string url = "https://example.com/largefile.zip";
+//        string filePath = "largefile.zip";
+
+//        Console.WriteLine("Начинается скачивание файла...");
+
+//        await DownloadFileAsync(url, filePath);
+
+//        Console.WriteLine("Скачивание завершено.");
+//    }
+
+//    static async Task DownloadFileAsync(string url, string filePath)
+//    {
+//        using (HttpClient client = new HttpClient())
+//        {
+//            using (HttpResponseMessage response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead))
+//            {
+//                response.EnsureSuccessStatusCode();
+
+//                var totalBytes = response.Content.Headers.ContentLength;
+
+//                using (var contentStream = await response.Content.ReadAsStreamAsync())
+//                using (var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None, 8192, true))
+//                {
+//                    var totalRead = 0L;
+//                    var buffer = new byte[8192];
+//                    var isMoreToRead = true;
+
+//                    do
+//                    {
+//                        var read = await contentStream.ReadAsync(buffer, 0, buffer.Length);
+//                        if (read == 0)
+//                        {
+//                            isMoreToRead = false;
+//                        }
+//                        else
+//                        {
+//                            await fileStream.WriteAsync(buffer, 0, read);
+//                            totalRead += read;
+
+//                            Console.WriteLine($"Скачано {totalRead} из {totalBytes} байт. {((double)totalRead / totalBytes.Value * 100):0.00}% завершено.");
+//                        }
+//                    }
+//                    while (isMoreToRead);
+//                }
+//            }
+//        }
+//    }
+//}
+
+
+
+// доп задача
+
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        int[] numbers = Enumerable.Range(1, 1000000).OrderByDescending(x => x).ToArray();
+//        Console.WriteLine("Начинается сортировка...");
+//        var sortedNumbers = MergeSort(numbers);
+//        Console.WriteLine("Сортировка завершена.");
+//        Console.WriteLine(string.Join(", ", sortedNumbers.Take(10)));
+//    }
+
+//    public static int[] MergeSort(int[] array)
+//    {
+//        if (array.Length <= 1)
+//            return array;
+
+//        int middle = array.Length / 2;
+//        int[] left = array.Take(middle).ToArray();
+//        int[] right = array.Skip(middle).ToArray();
+
+//        Task<int[]> leftTask = Task.Run(() => MergeSort(left));
+//        Task<int[]> rightTask = Task.Run(() => MergeSort(right));
+//        Task.WaitAll(leftTask, rightTask);
+
+//        return Merge(leftTask.Result, rightTask.Result);
+//    }
+
+//    public static int[] Merge(int[] left, int[] right)
+//    {
+//        int[] result = new int[left.Length + right.Length];
+//        int leftIndex = 0, rightIndex = 0, resultIndex = 0;
+
+//        while (leftIndex < left.Length && rightIndex < right.Length)
+//        {
+//            if (left[leftIndex] <= right[rightIndex])
+//            {
+//                result[resultIndex++] = left[leftIndex++];
+//            }
+//            else
+//            {
+//                result[resultIndex++] = right[rightIndex++];
+//            }
+//        }
+
+//        while (leftIndex < left.Length)
+//        {
+//            result[resultIndex++] = left[leftIndex++];
+//        }
+
+//        while (rightIndex < right.Length)
+//        {
+//            result[resultIndex++] = right[rightIndex++];
+//        }
+
+//        return result;
+//    }
+//}
+
+
+// доп задача 2
+
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        int[] numbers = Enumerable.Range(1, 100).ToArray();
+//        int numberOfThreads = Math.Max(1, numbers.Length / 10);
+
+//        Console.WriteLine($"Использование {numberOfThreads} потоков для подсчета суммы элементов массива.");
+
+//        int totalSum = CalculateSum(numbers, numberOfThreads);
+
+//        Console.WriteLine($"Итоговая сумма: {totalSum}");
+//    }
+
+//    static int CalculateSum(int[] array, int numberOfThreads)
+//    {
+//        int segmentLength = array.Length / numberOfThreads;
+//        Task<int>[] tasks = new Task<int>[numberOfThreads];
+
+//        for (int i = 0; i < numberOfThreads; i++)
+//        {
+//            int start = i * segmentLength;
+//            int end = (i == numberOfThreads - 1) ? array.Length : start + segmentLength;
+
+//            tasks[i] = Task.Run(() => SumSegment(array, start, end));
+//        }
+
+//        Task.WaitAll(tasks);
+
+//        int totalSum = tasks.Sum(task => task.Result);
+//        return totalSum;
+//    }
+
+//    static int SumSegment(int[] array, int start, int end)
+//    {
+//        int sum = 0;
+//        for (int i = start; i < end; i++)
+//        {
+//            sum += array[i];
+//        }
+//        return sum;
+//    }
+//}
---
+++ system_2.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        int numberOfCashRegisters = 3;
+//        int numberOfCustomers = 20;
+//        Random random = new Random();
+
+//        ConcurrentQueue<Customer> customerQueue = new ConcurrentQueue<Customer>();
+//        for (int i = 1; i <= numberOfCustomers; i++)
+//        {
+//            customerQueue.Enqueue(new Customer { Id = i });
+//        }
+
+//        Thread[] cashRegisterThreads = new Thread[numberOfCashRegisters];
+//        for (int i = 0; i < numberOfCashRegisters; i++)
+//        {
+//            CashRegister cashRegister = new CashRegister(i + 1, customerQueue, random);
+//            cashRegisterThreads[i] = new Thread(new ThreadStart(cashRegister.ProcessCustomers));
+//            cashRegisterThreads[i].Start();
+//        }
+
+//        foreach (Thread thread in cashRegisterThreads)
+//        {
+//            thread.Join();
+//        }
+
+//        Console.WriteLine("Все покупатели обслужены.");
+//    }
+//}
+
+//class Customer
+//{
+//    public int Id { get; set; }
+//}
+
+//class CashRegister
+//{
+//    private int Id { get; }
+//    private ConcurrentQueue<Customer> CustomerQueue { get; }
+//    private Random Random { get; }
+
+//    public CashRegister(int id, ConcurrentQueue<Customer> customerQueue, Random random)
+//    {
+//        Id = id;
+//        CustomerQueue = customerQueue;
+//        Random = random;
+//    }
+
+//    public void ProcessCustomers()
+//    {
+//        while (CustomerQueue.TryDequeue(out Customer customer))
+//        {
+//            Console.WriteLine($"Касса {Id}: Покупатель {customer.Id} начинает обслуживание.");
+//            int serviceTime = Random.Next(1, 4) * 1000;
+//            Thread.Sleep(serviceTime);
+//            Console.WriteLine($"Касса {Id}: Покупатель {customer.Id} заканчивает обслуживание (время: {serviceTime / 1000} секунд).");
+//        }
+//    }
+//}
+
+
+
+// доп задача
+
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        int numberOfClients = 5;
+//        List<Client> clients = new List<Client>();
+//        List<Thread> clientThreads = new List<Thread>();
+//        Random random = new Random();
+
+//        for (int i = 1; i <= numberOfClients; i++)
+//        {
+//            Account account = new Account { Balance = 1000 };
+//            Client client = new Client(i, account, random);
+//            clients.Add(client);
+
+//            Thread clientThread = new Thread(new ThreadStart(client.PerformOperations));
+//            clientThreads.Add(clientThread);
+//            clientThread.Start();
+//        }
+
+//        Thread.Sleep(30000);
+
+//        foreach (Client client in clients)
+//        {
+//            client.Stop();
+//        }
+
+//        foreach (Thread thread in clientThreads)
+//        {
+//            thread.Join();
+//        }
+
+//        Console.WriteLine("Программа завершена. Итоговое состояние счетов:");
+//        foreach (Client client in clients)
+//        {
+//            Console.WriteLine($"Клиент {client.Id}: Баланс {client.Account.Balance}");
+//        }
+//    }
+//}
+
+//class Account
+//{
+//    private decimal balance;
+//    private object balanceLock = new object();
+
+//    public decimal Balance
+//    {
+//        get
+//        {
+//            lock (balanceLock)
+//            {
+//                return balance;
+//            }
+//        }
+//        set
+//        {
+//            lock (balanceLock)
+//            {
+//                balance = value;
+//            }
+//        }
+//    }
+
+//    public void Deposit(decimal amount)
+//    {
+//        lock (balanceLock)
+//        {
+//            balance += amount;
+//        }
+//    }
+
+//    public bool Withdraw(decimal amount)
+//    {
+//        lock (balanceLock)
+//        {
+//            if (balance >= amount)
+//            {
+//                balance -= amount;
+//                return true;
+//            }
+//            else
+//            {
+//                return false;
+//            }
+//        }
+//    }
+//}
+
+//class Client
+//{
+//    public int Id { get; }
+//    public Account Account { get; }
+//    private Random Random { get; }
+//    private bool running;
+
+//    public Client(int id, Account account, Random random)
+//    {
+//        Id = id;
+//        Account = account;
+//        Random = random;
+//        running = true;
+//    }
+
+//    public void Stop()
+//    {
+//        running = false;
+//    }
+
+//    public void PerformOperations()
+//    {
+//        while (running)
+//        {
+//            int operation = Random.Next(0, 2);
+//            decimal amount = Random.Next(1, 101);
+
+//            if (operation == 0)
+//            {
+//                Account.Deposit(amount);
+//                Console.WriteLine($"Клиент {Id}: Пополнение на сумму {amount}. Текущий баланс: {Account.Balance}");
+//            }
+//            else
+//            {
+//                if (Account.Withdraw(amount))
+//                {
+//                    Console.WriteLine($"Клиент {Id}: Снятие на сумму {amount}. Текущий баланс: {Account.Balance}");
+//                }
+//                else
+//                {
+//                    Console.WriteLine($"Клиент {Id}: Недостаточно средств для снятия {amount}. Текущий баланс: {Account.Balance}");
+//                }
+//            }
+
+//            Thread.Sleep(Random.Next(1000, 3000));
+//        }
+//    }
+//}
